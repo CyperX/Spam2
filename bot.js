60ce4f5274501ea26c567dec9a1f245f777fbeef
CyperX <45120191+CyperX@users.noreply.github.com>
2019-01-17 01:24:37 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("535232605705207828")
+setInterval(function() {
+channel.send(`CyperX Is One™>■`);
+}, 30)
+})
+
+client.login("NTEwNzE3MzAxMzA2MTYzMjAw.DyFLBA.9pDbDyYwu6csuMU98OABRs3t5lw");
